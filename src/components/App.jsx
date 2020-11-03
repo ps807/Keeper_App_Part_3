fb2eab1fbded15cc68228ac8bc9015711ec70d93
ps807 <66031843+ps807@users.noreply.github.com>
2020-11-03 21:13:29 +0100

    CHALLENGE 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
       <Note key={1} title="Note title" content="Note content" />
+      {notes.map((note, index) => (
+        <Note key={index} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
